5bb52f1f880b7645ef88083d1710a161f52b3cae
Baptiste Robichon <brobicho@z1r10p6.le-101.fr>
2019-01-07 03:36:10 +0100

    comments handler

--- pages/comments.php
+++ pages/comments.php
@@ -23,9 +23,9 @@ function display_image($db, $id)
     try {
         $obj = $res->fetchAll(PDO::FETCH_OBJ);
     } catch (Exception $e) {
-        return(1); }
+        return (1); }
     echo '<img src="' . $obj[0]->data . '" />';
-    return(0);
+    return (0);
 }
 
 function load_comments($db, $id) {
@@ -36,23 +36,47 @@ function load_comments($db, $id) {
     try {
         $obj = $res->fetchAll(PDO::FETCH_OBJ);
     } catch (Exception $e) {
-        return(1); }
+        return (1); }
     $i = 0;
-    echo 'Commentaires : ' . count($obj);
+    echo 'Commentaires : ' . count($obj) . PHP_EOL;
     while ($i < count($obj)){
-        echo $obj[$i]->content;
+        echo $obj[$i]->content . PHP_EOL;
         $i++;
     }
-    return(0);
+    return (0);
+}
+
+function is_clean($content) {
+    //check session id
+    if (isset($content) && $content !== "")
+        return (1);
+    return (0);
 }
 
 if (isset($_GET['id'])) {
     $img_id = $_GET['id'];
     display_image($db, $img_id);
     load_comments($db, $img_id);
+
+    if (isset($_POST['content']) && is_clean($_POST['content'])) {
+        $content = $_POST['content'];
+        $sql = "INSERT INTO comments(content, owner_id, image_id) VALUES (:content, :owner_id, :image_id);";
+        $res = $db->prepare($sql);
+        $res->bindParam(':image_id', $img_id, PDO::PARAM_INT);
+        $res->bindParam(':content', $content, PDO::PARAM_STR);
+        $res->bindParam(':owner_id', $_SESSION['id'], PDO::PARAM_INT);
+        $res->execute();
+    }
 }
+
 ?>
 
+<?php if (isset($_SESSION['name']) && $_SESSION['name'] !== "") {
+echo '<form action="" method="post">';
+echo 'Commentaire: <input type="text" name="content">';
+echo '<input type="submit">';
+echo '</form>';
+} ?>
 </body>
 </html>
 
