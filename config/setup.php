c26e86ae0c77908afdf429b38daba13cc4654e8b
brobicho <brobicho@student.le-101.fr>
2019-02-10 11:59:22 +0100

    fixed install

--- config/setup.php
+++ config/setup.php
@@ -5,14 +5,24 @@ function add_admin($db, $nom, $prenom, $mdp) {
     $sql = "SELECT * FROM users WHERE mail = 'admin@mail.fr'";
     $res = $db->query($sql);
     if (!($res->fetch(PDO::FETCH_OBJ))) {
-        $sql = "INSERT INTO users(nom, prenom, mdp, mail, admin) VALUES ('$nom', '$prenom', '$mdp', 'admin@mail.fr', '2')";
+        $sql = "INSERT INTO users(nom, prenom, mdp, mail, admin, henc) VALUES ('$nom', '$prenom', '$mdp', 'admin@mail.fr', '1', '0')";
         $db->query($sql);
     }
 }
 
 function create_users_table($db) {
     try {
-        $sql = "CREATE TABLE IF NOT EXISTS users(id INT NOT NULL PRIMARY KEY AUTO_INCREMENT, nom VARCHAR(64) NOT NULL, prenom VARCHAR(64) NOT NULL, mdp VARCHAR(64) NOT NULL, mail VARCHAR(64) UNIQUE NOT NULL, admin INT NOT NULL)";
+        $sql = "DROP TABLE IF EXISTS `users`;
+			CREATE TABLE IF NOT EXISTS `users` (
+			`id` int(11) NOT NULL AUTO_INCREMENT,
+			`nom` varchar(64) COLLATE latin1_general_ci NOT NULL,
+			`prenom` varchar(64) COLLATE latin1_general_ci NOT NULL,
+			`mdp` varchar(64) COLLATE latin1_general_ci NOT NULL,
+			`mail` varchar(64) COLLATE latin1_general_ci NOT NULL,
+			`admin` int(11) NOT NULL,
+			`henc` varchar(64) COLLATE latin1_general_ci NOT NULL,
+			PRIMARY KEY (`id`),
+			UNIQUE KEY `mail` (`mail`)) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci;";
         $db->query($sql);
         add_admin($db, 'admin', 'admin', hash('sha256', 'admin'));
     }
@@ -24,15 +34,13 @@ function create_users_table($db) {
 
 function create_gallery_table($db) {
     try {
-    $sql = "CREATE TABLE IF NOT EXISTS `gallery` (
-        `id` int(11) NOT NULL,
-        `owner_id` int(11) NOT NULL,
-        `data` longtext CHARACTER SET utf8 NOT NULL,
-        `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-        `likes` int(11) NOT NULL
-        ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-        ALTER TABLE `gallery` ADD PRIMARY KEY (`id`);
-        ALTER TABLE `gallery` MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;";
+    $sql = "DROP TABLE IF EXISTS `gallery`;
+		CREATE TABLE IF NOT EXISTS `gallery` (
+		`id` int(11) NOT NULL AUTO_INCREMENT,
+		`owner_id` int(11) NOT NULL,
+		`data` longtext COLLATE latin1_general_ci NOT NULL,
+		`likes` int(11) NOT NULL,
+		PRIMARY KEY (`id`)) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci;";
     $db->query($sql);
     }
     catch(PDOException $e) {
@@ -44,8 +52,13 @@ function create_gallery_table($db) {
 function create_comments_table($db)
 {
     try {
-        $sql = "CREATE TABLE `camagru_db`.`comments` ( `id` INT NOT NULL AUTO_INCREMENT , `content` LONGTEXT NOT NULL , `owner_id` INT NOT NULL , PRIMARY KEY (`id`)) ENGINE = InnoDB
-                ALTER TABLE `comments` ADD `image_id` INT NOT NULL AFTER `owner_id`;";
+        $sql = "DROP TABLE IF EXISTS `comments`;
+			CREATE TABLE IF NOT EXISTS `comments` (
+			`id` int(11) NOT NULL AUTO_INCREMENT,
+			`content` longtext COLLATE latin1_general_ci NOT NULL,
+			`owner_id` int(11) NOT NULL,
+			`image_id` int(11) NOT NULL,
+			PRIMARY KEY (`id`)) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci;";
         $db->query($sql);
     }
     catch(PDOException $e) {
@@ -56,8 +69,14 @@ function create_comments_table($db)
 
 function create_likes_table($db) {
     try {
-        $sql = "CREATE TABLE `camagru_db`.`likes` ( `id` INT NOT NULL AUTO_INCREMENT , `owner` INT NOT NULL , `image` INT NOT NULL , `date` TIMESTAMP on update CURRENT_TIMESTAMP NOT NULL , PRIMARY KEY (`id`)) ENGINE = InnoDB";
-        $db->query($sql);
+        $sql = "DROP TABLE IF EXISTS `likes`;
+			CREATE TABLE IF NOT EXISTS `likes` (
+			`id` int(11) NOT NULL AUTO_INCREMENT,
+			`owner` int(11) NOT NULL,
+			`image` int(11) NOT NULL,
+			`date` TIMESTAMP on update CURRENT_TIMESTAMP NOT NULL,
+			PRIMARY KEY (`id`)) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci;";
+		$db->query($sql);
     }
     catch(PDOException $e) {
         echo "Error: " . $e . PHP_EOL;
